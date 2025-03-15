c06d336fe03fd48fb4dfc2737d6de9771b196be6
adelille <adelille@student.42.fr>
2025-03-15 11:06:46 +0100

    simple loading

--- app/front/src/app.rs
+++ app/front/src/app.rs
@@ -2,18 +2,28 @@ use leptos::prelude::*;
 
 #[component]
 pub fn App() -> impl IntoView {
-	let (count, set_count) = signal(0);
+	let (index, set_index) = signal(0);
+	let (text, set_text) = signal(None);
+
+	set_text.set(Some("Hello, World!".to_string()));
+
+	/*
+	if text.get().is_none() {
+		return view! {
+			<p>
+				"Loading..."
+			</p>
+		};
+	}
+	*/
 
 	view! {
-		<button
-			on:click=move |_| set_count.set(count.get() + 1)
-		>
-			"Click me: "
-			{count}
-		</button>
-		<p>
-			"Double count: "
-			{move || count.get() * 2}
-		</p>
+		<button on:click=move |_| *set_index.write() += 1>"Click me: " {index}</button>
+		<p>"Double index: " {move || index.get() * 2}</p>
+		<p>{text}</p>
+		<progress
+			max=move || text.get().expect("text is None").len()
+			value=index
+		/>
 	}
 }
