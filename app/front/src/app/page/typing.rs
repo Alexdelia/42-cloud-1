d5af5c9225c5144ae56f04e2ed56135697721587
adelille <adelille@student.42.fr>
2025-03-15 13:04:43 +0100

    simple fix

--- app/front/src/app/page/typing.rs
+++ app/front/src/app/page/typing.rs
@@ -9,14 +9,14 @@ pub fn Typing(text: ReadSignal<String>) -> impl IntoView {
 			set_index.set((index.get() + 1) % text.get().len())
 		}>"Click me: " {index}</button>
 		<Field text=text index=index />
-		<progress max=move || text.get().len() value=index />
+		<progress max=move || text.get().len() - 1 value=index />
 	}
 }
 
 #[component]
 fn Field(text: ReadSignal<String>, index: ReadSignal<usize>) -> impl IntoView {
 	view! {
-		<div>
+		<div style="display: flex; justify-content: center">
 			<span>{move || text.get()[..index.get()].to_string()}</span>
 			<span style="font-weight: bold">
 				{move || {
