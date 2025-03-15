3f638878f5f129326c4107712b8141bd08d41484
adelille <adelille@student.42.fr>
2025-03-15 10:50:37 +0100

    simple btn example

--- app/front/src/main.rs
+++ app/front/src/main.rs
@@ -1,10 +1,6 @@
-use leptos::prelude::*;
+mod app;
 
 fn main() {
 	console_error_panic_hook::set_once();
-	leptos::mount::mount_to_body(|| {
-		view! {
-			<p>"Hello, world from Rust!"</p>
-		}
-	})
+	leptos::mount::mount_to_body(app::App);
 }
