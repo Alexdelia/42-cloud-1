c98582337bd2bd564cb9b11fb71cba1b9795ff65
adelille <adelille@student.42.fr>
2025-03-15 10:22:42 +0100

    some improvement

--- app/front/src/lib.rs
+++ app/front/src/lib.rs
@@ -2,28 +2,32 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn on_keydown(event: web_sys::KeyboardEvent) {
-    // Log the key that was pressed
     web_sys::console::log_1(&event.key().into());
-}
 
-// Called when the Wasm module is instantiated
-#[wasm_bindgen(start)]
-fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+    let field = document
+        .get_element_by_id("typing")
+        .expect("cannot find element with id 'typing'");
 
-    body.append_child(&val)?;
+    field.set_text_content(Some(&event.key()));
+}
+
+#[wasm_bindgen(start)]
+fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    // let body = document.body().expect("document should have a body");
 
     let c = Closure::wrap(Box::new(on_keydown) as Box<dyn FnMut(_)>);
     window.add_event_listener_with_callback("keydown", c.as_ref().unchecked_ref())?;
     c.forget();
+
+    let field = document
+        .get_element_by_id("typing")
+        .expect("cannot find element with id 'typing'");
+    field.set_text_content(Some("Press any key"));
 
     Ok(())
 }
