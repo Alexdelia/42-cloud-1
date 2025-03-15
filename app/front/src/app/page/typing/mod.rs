cf229c79c9800f0bff7250ada4453f7e4386e562
adelille <adelille@student.42.fr>
2025-03-15 19:42:06 +0100

    the animation is working!!!

--- app/front/src/app/page/typing/mod.rs
+++ app/front/src/app/page/typing/mod.rs
@@ -8,8 +8,16 @@ stylance::import_crate_style!(style, "src/app/page/typing/style.module.scss");
 pub fn Typing() -> impl IntoView {
 	let (text, set_text) = signal("If this is all it takes to tear us apart, then maybe we weren't all that close to begin with.".to_string().replace(" ", "_"));
 	let (index, set_index) = signal(0usize);
+	let (animations, set_animations) = signal(Vec::<(String, usize)>::new());
+	let animation_id = create_rw_signal(0);
 
-	on_keydown::set_event_listener(text.clone(), index.clone(), set_index);
+	on_keydown::set_event_listener(
+		text.clone(),
+		index.clone(),
+		set_index,
+		set_animations,
+		animation_id,
+	);
 
 	view! {
 		<div class=style::container>
@@ -20,7 +28,33 @@ pub fn Typing() -> impl IntoView {
 				<field::Field text=text index=index />
 
 				<progress max=move || text.get().len() - 1 value=index />
+
+				<WrongKeyAnimations animations=animations />
 			</Show>
+		</div>
+	}
+}
+
+#[component]
+fn WrongKeyAnimations(animations: ReadSignal<Vec<(String, usize)>>) -> impl IntoView {
+	view! {
+		<div class=style::animation_container>
+			<For
+				each=move || animations.get()
+				key=|(_, id)| *id
+				children=move |(key, _)| {
+					view! {
+						<div
+							class=style::wrong_key_animation
+							style=format!(
+								"left: 50vw; top: 70vh",
+							)
+						>
+							{key}
+						</div>
+					}
+				}
+			/>
 		</div>
 	}
 }
--- app/front/src/app/page/typing/on_keydown.rs
+++ app/front/src/app/page/typing/on_keydown.rs
@@ -4,6 +4,8 @@ pub fn set_event_listener(
 	text: ReadSignal<String>,
 	index: ReadSignal<usize>,
 	set_index: WriteSignal<usize>,
+	set_animations: WriteSignal<Vec<(String, usize)>>,
+	animation_id: RwSignal<usize>,
 ) {
 	let handle = window_event_listener(leptos::ev::keydown, move |event| {
 		let mut key = event.key();
@@ -23,6 +25,17 @@ pub fn set_event_listener(
 
 		if key != target.to_string() {
 			leptos::logging::log!("Key pressed: '{key}' != '{target}'");
+
+			let id = animation_id.get();
+			animation_id.update(|id| *id += 1);
+			set_animations.update(|animations| animations.push((key.clone(), id)));
+			set_timeout(
+				move || {
+					set_animations
+						.update(|animations| animations.retain(|(_, anim_id)| *anim_id != id));
+				},
+				std::time::Duration::from_secs(2),
+			);
 			return;
 		}
 
