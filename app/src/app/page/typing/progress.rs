4350391d62c742569825818322b7a8e0595d453f
adelille <adelille@student.42.fr>
2025-03-16 22:32:11 +0100

    transtioned progress bar

--- app/src/app/page/typing/progress.rs
+++ app/src/app/page/typing/progress.rs
@@ -1,7 +1,5 @@
 use leptos::prelude::*;
 
-stylance::import_crate_style!(style, "src/app/page/typing/progress.module.css");
-
 #[component]
 pub fn Progress(value: ReadSignal<usize>, max: usize) -> impl IntoView {
 	view! { <progress value=value max=max /> }
