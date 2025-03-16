c78168f44974fcd1b72eac86c68f27f620b9f31c
adelille <adelille@student.42.fr>
2025-03-16 22:34:27 +0100

    transition wrong key animation

--- app/src/app/page/typing/wrong_key_animation.rs
+++ app/src/app/page/typing/wrong_key_animation.rs
@@ -2,19 +2,17 @@ use leptos::prelude::*;
 
 use super::Animation;
 
-stylance::import_crate_style!(style, "src/app/page/typing/wrong_key_animation.module.css");
-
 #[component]
 pub fn WrongKeyAnimations(animations: ReadSignal<Vec<Animation>>) -> impl IntoView {
 	view! {
-		<div class=style::animation_container>
+		<div class="wrong_key_animation_container">
 			<For
 				each=move || animations.get()
 				key=|(_, id)| *id
 				children=move |(key, id)| {
 					view! {
 						<div
-							class=style::wrong_key_animation
+							class="wrong_key_animation"
 							style=format!("z-index: {}", 8 + id)
 						>
 							{key}
