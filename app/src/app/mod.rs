e093c5f7eebe00813e795a5d73669fc9936d66de
adelille <adelille@student.42.fr>
2025-03-16 22:15:26 +0100

    connect previously existing page

--- app/src/app/mod.rs
+++ app/src/app/mod.rs
@@ -1,7 +1,51 @@
-use leptos::prelude::*;
 mod page;
+
+use leptos::prelude::*;
+use leptos_meta::{MetaTags, Stylesheet, Title, provide_meta_context};
+use leptos_router::{
+	StaticSegment,
+	components::{Route, Router, Routes},
+};
+
+pub fn shell(options: LeptosOptions) -> impl IntoView {
+	view! {
+		<!DOCTYPE html> 
+		<html lang="en">
+			<head>
+				<meta charset="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<AutoReload options=options.clone() />
+				<HydrationScripts options />
+				<MetaTags />
+			</head>
+			<body>
+				<App />
+			</body>
+		</html>
+	}
+}
 
 #[component]
 pub fn App() -> impl IntoView {
-	view! { <page::Typing /> }
+	// Provides context that manages stylesheets, titles, meta tags, etc.
+	provide_meta_context();
+
+	view! {
+		<Stylesheet id="leptos" href="/pkg/cloud-1.css" />
+
+		// sets the document title
+		<Title text="Welcome to Leptos" />
+
+		<Router>
+			<main>
+				// TODO: add a nice 404 page
+				<Routes fallback=|| "Page not found.".into_view()>
+					<Route path=StaticSegment("") view=page::Typing />
+				</Routes>
+			</main>
+		</Router>
+	}
 }
