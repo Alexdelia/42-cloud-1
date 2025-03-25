97b68e760a4368d09a480cd89418b6dda4d64b30
adelille <adelille@student.42.fr>
2025-03-25 20:41:56 +0100

    crane src

--- app/default.nix
+++ app/default.nix
@@ -2,8 +2,22 @@
   pkgs,
   inputs,
 }: let
+  lib = pkgs.lib;
   craneLib = inputs.crane.mkLib pkgs;
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      (craneLib.fileset.commonCargoSources ./.)
+      (
+        lib.fileset.fileFilter
+        (file: lib.any file.hasExt ["html" "scss"])
+        ./.
+      )
+      (lib.fileset.maybeMissing ./public)
+    ];
+  };
 in
   craneLib.buildPackage {
-    src = craneLib.cleanCargoSource ./.;
+    inherit src;
   }
