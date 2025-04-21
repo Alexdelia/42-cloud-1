65a9e2f88b1c5f2db102296403a53b94e4fdcc82
adelille <adelille@student.42.fr>
2025-04-21 16:05:15 +0200

    organize + insert query

--- app/src/schema/stats/mod.rs
+++ app/src/schema/stats/mod.rs
@@ -4,11 +4,8 @@ mod query;
 
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-#[cfg(feature = "ssr")]
-use sqlx::prelude::FromRow;
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
-#[cfg_attr(feature = "ssr", derive(FromRow))]
 pub struct Stats {
 	pub start_time: DateTime<Utc>,
 	pub end_time: DateTime<Utc>,
@@ -27,38 +24,4 @@ impl Default for Stats {
 			wrong_key: 0,
 		}
 	}
-}
-
-cfg_if::cfg_if! {
-if #[cfg(feature = "ssr")] {
-
-use uuid::Uuid;
-
-#[derive(Clone, Serialize, Deserialize, Debug)]
-#[cfg_attr(feature = "ssr", derive(FromRow))]
-pub struct StatsRow {
-	pub uuid: Uuid,
-	pub user_uuid: Uuid,
-
-	pub start_time: DateTime<Utc>,
-	pub end_time: DateTime<Utc>,
-
-	pub correct_key: i32,
-	pub wrong_key: i32,
-
-	pub created_at: DateTime<Utc>,
-}
-
-impl From<StatsRow> for Stats {
-	fn from(row: StatsRow) -> Self {
-		Self {
-			start_time: row.start_time,
-			end_time: row.end_time,
-
-			correct_key: row.correct_key as usize,
-			wrong_key: row.wrong_key as usize,
-		}
-	}
-}
-}
 }
--- app/src/schema/stats/query.rs
+++ app/src/schema/stats/query.rs
@@ -1,5 +1,36 @@
-use super::{Stats, StatsRow};
+use super::Stats;
+use chrono::{DateTime, Utc};
 use leptos::prelude::*;
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+use uuid::Uuid;
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+#[cfg_attr(feature = "ssr", derive(FromRow))]
+pub struct StatsRow {
+	pub uuid: Uuid,
+	pub user_uuid: Uuid,
+
+	pub start_time: DateTime<Utc>,
+	pub end_time: DateTime<Utc>,
+
+	pub correct_key: i32,
+	pub wrong_key: i32,
+
+	pub created_at: DateTime<Utc>,
+}
+
+impl From<StatsRow> for Stats {
+	fn from(row: StatsRow) -> Self {
+		Self {
+			start_time: row.start_time,
+			end_time: row.end_time,
+
+			correct_key: row.correct_key as usize,
+			wrong_key: row.wrong_key as usize,
+		}
+	}
+}
 
 #[server]
 pub async fn list() -> Result<Vec<Stats>, ServerFnError> {
@@ -16,22 +47,22 @@ pub async fn list() -> Result<Vec<Stats>, ServerFnError> {
 	Ok(stats)
 }
 
-/*
 #[server]
-pub async fn store(cx: Scope, stats: Stats) -> Result<(), ServerFnError> {
-	let pool = cx.get::<sqlx::PgPool>().await?;
-	sqlx::query(
-		"INSERT INTO stats (start_time, end_time, correct_key, wrong_key) VALUES ($1, $2, $3, $4)",
-	)
-	.bind((
+pub async fn store(user_uuid: Uuid, stats: Stats) -> Result<(), ServerFnError> {
+	let pool = use_context::<sqlx::PgPool>().ok_or_else::<ServerFnError, _>(|| {
+		ServerFnError::ServerError(String::from("no postgres connection pool"))
+	})?;
+
+	sqlx::query!(
+        "INSERT INTO stats (user_uuid, start_time, end_time, correct_key, wrong_key) VALUES ($1, $2, $3, $4, $5)",
+        user_uuid,
 		stats.start_time,
 		stats.end_time,
-		stats.correct_key,
-		stats.wrong_key,
-	))
+		stats.correct_key as i32,
+		stats.wrong_key as i32,
+	)
 	.execute(&pool)
 	.await?;
 
 	Ok(())
 }
-*/
