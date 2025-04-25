0a9a73f653dde6561552a1566ea18bd9e0350c23
Alexandre Delille <alexandre@terros.io>
2025-04-25 21:14:38 +0200

    working personal best loading

--- app/src/app/page/stats/history/personal_best.rs
+++ app/src/app/page/stats/history/personal_best.rs
@@ -3,28 +3,13 @@ use uuid::Uuid;
 
 #[component]
 pub fn PersonalBest(user_uuid: Uuid) -> impl IntoView {
-	/*
 	let pb = Resource::new(
 		move || user_uuid,
 		move |user_uuid| crate::schema::stats::query::personal_best(user_uuid),
 	);
 
 	view! {
-		<super::row::Row row=Default::default() />
-
-		<Transition fallback=move || {
-			view! { <p>"Loading initial data..."</p> }
-		}>
-			<Show
-				when=move || { match pb.get() {
-					Some(Ok(Some(_))) => true,
-					_ => false,
-				}}
-				fallback=move || {
-					view! { <p>"No personal best yet..."</p> }
-				}
-			>
-
+		<Transition>
 			{move || {
 				(match pb.get() {
 					Some(Ok(Some(row))) => Some(row),
@@ -34,12 +19,6 @@ pub fn PersonalBest(user_uuid: Uuid) -> impl IntoView {
 						view! { <super::row::Row row=row /> }
 					})
 			}}
-			</Show>
 		</Transition>
-	}
-	*/
-
-	view! {
-		<super::row::Row row=Default::default() />
 	}
 }
