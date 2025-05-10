cffb4b55ba795537c28c4ce7d04be8f413985d64
adelille <adelille@student.42.fr>
2025-05-10 23:47:10 +0200

    nice number formatting

--- app/src/app/page/stats/global_compare/mod.rs
+++ app/src/app/page/stats/global_compare/mod.rs
@@ -87,22 +87,20 @@ pub fn GlobalCompare(user_uuid: Uuid) -> impl IntoView {
 									<StatsBubble
 										title="key sum".to_owned()
 										user=view! {
-											// TODO: locale format
-											<span>{format!("{}", stats.user.key_sum)}</span>
+											<span>{format_key_sum(stats.user.key_sum)}</span>
 										}
 										global=view! {
-											<span>{format!("{}", stats.global.key_sum)}</span>
+											<span>{format_key_sum(stats.global.key_sum)}</span>
 										}
 									/>
 
 									<StatsBubble
 										title="time sum".to_owned()
 										user=view! {
-											// TODO: locale format
-											<span>{format!("{}", stats.user.time_sum)}</span>
+											<span>{format_time_sum(stats.user.time_sum)}</span>
 										}
 										global=view! {
-											<span>{format!("{}", stats.global.time_sum)}</span>
+											<span>{format_time_sum(stats.global.time_sum)}</span>
 										}
 									/>
 								</div>
@@ -111,5 +109,22 @@ pub fn GlobalCompare(user_uuid: Uuid) -> impl IntoView {
 					})
 			}}
 		</Transition>
+	}
+}
+
+fn format_time_sum(time_sum: f32) -> String {
+	let h = (time_sum / 3600.0).floor();
+	let m = ((time_sum % 3600.0) / 60.0).floor();
+	let s = (time_sum % 60.0).floor();
+	format!("{h:02}:{m:02}:{s:02}")
+}
+
+fn format_key_sum(key_sum: i64) -> String {
+	if key_sum > 1_000_000 {
+		format!("{:.1}m", key_sum as f64 / 1_000_000.0)
+	} else if key_sum > 1_000 {
+		format!("{:.1}k", key_sum as f64 / 1_000.0)
+	} else {
+		key_sum.to_string()
 	}
 }
